2d1f12a9749bbd03134ca22bf1d8b94aa9de79dc
Huy Quang Ngo <manhle@Huys-Mac-mini.local>
2015-09-05 11:59:16 +0700

    saaa

--- FlappBirdDemo/FlappBirdDemo/class/PlaySence.m
+++ FlappBirdDemo/FlappBirdDemo/class/PlaySence.m
@@ -23,24 +23,80 @@
 @implementation PlaySence{
     UIImageView *bird;
     NSTimer* timer;
-    CGFloat speedValue;
+    CGFloat speedValue,valueRandom , birdValueChange,jumpVelocity, fallAcceleration;;
     CGRect sizeMain;
+    BOOL isJump;
+    CGAffineTransform rotation,scale;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    speedValue=10;
+    isJump=false;
+    speedValue=0.8;
+    valueRandom=0;
+    birdValueChange=0;
+    jumpVelocity=1.0;
     sizeMain=[[UIScreen mainScreen] bounds];
     [self Addsprite];
-    [self animationBrid];
-    timer=[NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(running) userInfo:nil repeats:true];
+    [self.view addGestureRecognizer:[[UITapGestureRecognizer alloc]
+                     initWithTarget:self
+                             action:@selector(birdFlight)]];
+    
+    timer=[NSTimer scheduledTimerWithTimeInterval:0.01 target:self selector:@selector(running) userInfo:nil repeats:true];
 }
--(void) animationBrid{
-//    [UIView animateWithDuration:2 animations:^{
-//        <#code#>
-//    } completion:^(BOOL finished) {
-//        <#code#>
-//    }];
+-(void)birdFlight{
+    jumpVelocity=1.0;
+    CGRect f=bird.frame;
+    if (f.origin.y<=100) {
+        f.origin.y=-35;
+    }else
+        f.origin.y-=100;
+    
+    [UIView animateWithDuration:0.5 animations:^{
+//        if (!isJump) {
+//            bird.transform = CGAffineTransformMakeRotation(-M_PI/2);
+//        }
+        bird.frame=f;
+    } completion:^(BOOL finished) {
+//        isJump=false;
+//        [UIView animateWithDuration:0.5 animations:^{
+//            bird.transform = CGAffineTransformMakeRotation(M_PI/2);
+//        }];
+    }];
+}
+-(void)ProcessBird{
+    CGRect f=bird.frame;
+    jumpVelocity+=0.01;
+    f.origin.y+=jumpVelocity;
+    bird.frame=f;
+}
+-(void)checkEndGame{
+    CGRect b=[bird convertRect:bird.bounds toView:nil];
+    CGRect o1=[_ongDuoi1 convertRect:_ongDuoi1.bounds toView:nil];
+    CGRect o2=[_ongDuoi2 convertRect:_ongDuoi2.bounds toView:nil];
+    CGRect o3=[_ongDuoi3 convertRect:_ongDuoi3.bounds toView:nil];
+    CGRect o4=[_ongTren1 convertRect:_ongTren1.bounds toView:nil];
+    CGRect o5=[_ongTren2 convertRect:_ongTren2.bounds toView:nil];
+    CGRect o6=[_ongTren3 convertRect:_ongTren3.bounds toView:nil];
+    CGRect n=[_ground convertRect:_ground.bounds toView:nil];
+    BOOL od1=CGRectIntersectsRect(b,o1);
+    BOOL od2=CGRectIntersectsRect(b, o2);
+    BOOL od3=CGRectIntersectsRect(b, o3);
+    BOOL ot1=CGRectIntersectsRect(b, o4);
+    BOOL ot2=CGRectIntersectsRect(b, o5);
+    BOOL ot3=CGRectIntersectsRect(b, o6);
+    BOOL nen=CGRectIntersectsRect(b, n);
+    if(od1||ot1){
+        [timer invalidate];
+//        [UIView animateWithDuration:0.8 animations:^{
+//            CGRect f=bird.frame;
+//            f.origin.y=sizeMain.size.height+20;
+//            bird.frame=f;
+//        } completion:^(BOOL finished) {
+//            [bird removeFromSuperview];
+//            [self viewDidLoad];
+//        }];
+    }
 }
 -(void)running{
     //chay background
@@ -61,53 +117,60 @@
     }
     
     //chay ong duoi va ong tren
-    _ongDuoi1.frame=CGRectMake(_ongDuoi1.frame.origin.x-speedValue, _ongDuoi1.frame.origin.y, _ongDuoi1.frame.size.width, _ongDuoi1.frame.size.height);
-    _ongDuoi2.frame=CGRectMake(_ongDuoi2.frame.origin.x-speedValue, _ongDuoi2.frame.origin.y, _ongDuoi2.frame.size.width, _ongDuoi2.frame.size.height);
-    _ongDuoi3.frame=CGRectMake(_ongDuoi3.frame.origin.x-speedValue, _ongDuoi3.frame.origin.y, _ongDuoi3.frame.size.width, _ongDuoi3.frame.size.height);
-    
-    if (_ongDuoi1.frame.origin.x+_ongDuoi1.frame.size.width<0) {
-        _ongDuoi1.frame=CGRectMake(_ongDuoi3.frame.origin.x +_ongDuoi3.frame.size.width+200, _ongDuoi3.frame.origin.y,_ongDuoi1.frame.size.width, _ongDuoi1.frame.size.height);
-    }
-    if (_ongDuoi2.frame.origin.x+_ongDuoi2.frame.size.width<0) {
-        _ongDuoi2.frame=CGRectMake(_ongDuoi1.frame.origin.x +_ongDuoi1.frame.size.width+200, _ongDuoi2.frame.origin.y,_ongDuoi2.frame.size.width, _ongDuoi2.frame.size.height);
-    }
-    if (_ongDuoi3.frame.origin.x+_ongDuoi3.frame.size.width<0) {
-        _ongDuoi3.frame=CGRectMake(_ongDuoi2.frame.origin.x +_ongDuoi2.frame.size.width+200, _ongDuoi3.frame.origin.y,_ongDuoi3.frame.size.width, _ongDuoi3.frame.size.height);
-    }
-    
-    _ongTren1.frame=CGRectMake(_ongTren1.frame.origin.x-speedValue, _ongTren1.frame.origin.y, _ongTren1.frame.size.width, _ongTren1.frame.size.height);
-    _ongTren2.frame=CGRectMake(_ongTren2.frame.origin.x-speedValue, _ongTren2.frame.origin.y, _ongTren2.frame.size.width, _ongTren2.frame.size.height);
-    _ongTren3.frame=CGRectMake(_ongTren3.frame.origin.x-speedValue, _ongTren3.frame.origin.y, _ongTren3.frame.size.width, _ongTren3.frame.size.height);
-    
     if (_ongTren1.frame.origin.x+_ongTren1.frame.size.width<0) {
-        _ongTren1.frame=CGRectMake(_ongTren3.frame.origin.x +_ongTren3.frame.size.width+200, _ongTren1.frame.origin.y,_ongTren1.frame.size.width, _ongTren1.frame.size.height);
+         valueRandom=(CGFloat)arc4random_uniform(350)-450;
+        _ongTren1.frame=CGRectMake(_ongTren3.frame.origin.x +_ongTren3.frame.size.width+200,valueRandom,_ongTren1.frame.size.width, _ongTren1.frame.size.height);
+    }
+    else{
+        _ongTren1.frame=CGRectMake(_ongTren1.frame.origin.x-speedValue, _ongTren1.frame.origin.y, _ongTren1.frame.size.width, _ongTren1.frame.size.height);
     }
     if (_ongTren2.frame.origin.x+_ongTren2.frame.size.width<0) {
-        _ongTren2.frame=CGRectMake(_ongTren1.frame.origin.x +_ongTren1.frame.size.width+200, _ongDuoi2.frame.origin.y,_ongTren2.frame.size.width, _ongTren2.frame.size.height);
+         valueRandom=(CGFloat)arc4random_uniform(350)-450;
+        _ongTren2.frame=CGRectMake(_ongTren1.frame.origin.x +_ongTren1.frame.size.width+200, -100,_ongTren2.frame.size.width, _ongTren2.frame.size.height);
+    }else{
+         _ongTren2.frame=CGRectMake(_ongTren2.frame.origin.x-speedValue, _ongTren2.frame.origin.y, _ongTren2.frame.size.width, _ongTren2.frame.size.height);
     }
+    
     if (_ongTren3.frame.origin.x+_ongTren3.frame.size.width<0) {
-        _ongTren3.frame=CGRectMake(_ongTren1.frame.origin.x +_ongTren1.frame.size.width+200, _ongTren3.frame.origin.y,_ongTren3.frame.size.width, _ongTren3.frame.size.height);
+        valueRandom=(CGFloat)arc4random_uniform(350)-450;
+        _ongTren3.frame=CGRectMake(_ongTren2.frame.origin.x +_ongTren2.frame.size.width+200, valueRandom,_ongTren3.frame.size.width, _ongTren3.frame.size.height);
+    }else{
+          _ongTren3.frame=CGRectMake(_ongTren3.frame.origin.x-speedValue, _ongTren3.frame.origin.y, _ongTren3.frame.size.width, _ongTren3.frame.size.height);
     }
-    
-    
 
+    _ongDuoi1.frame=CGRectMake(_ongTren1.frame.origin.x,_ongTren1.frame.origin.y
+                               +_ongTren1.frame.size.height+100, _ongDuoi1.frame.size.width, _ongDuoi1.frame.size.height);
+    
+    _ongDuoi2.frame=CGRectMake(_ongTren2.frame.origin.x,_ongTren2.frame.origin.y
+                               +_ongTren2.frame.size.height+100,  _ongDuoi2.frame.size.width,  _ongDuoi2.frame.size.height);
+    
+    _ongDuoi3.frame=CGRectMake(_ongTren3.frame.origin.x, _ongTren3.frame.origin.y
+                               +_ongTren3.frame.size.height+100,  _ongDuoi3.frame.size.width,  _ongDuoi3.frame.size.height);
+    
+    //su ly chim
+    [self ProcessBird];
+    [self checkEndGame];
 }
+
 -(void)Addsprite{
 
     _bg1.frame=CGRectMake(0, 0, _bg1.frame.size.width, _bg1.frame.size.height);
-    _bg2.frame=CGRectMake(sizeMain.size.width, 0, _bg2.frame.size.width, _bg2.frame.size.height);
+    _bg2.frame=CGRectMake(sizeMain.size.width,0, _bg2.frame.size.width, _bg2.frame.size.height);
 
-    _ongTren1.frame=CGRectMake(sizeMain.size.width, 0, 90, 277);
+    _ongTren1.frame=CGRectMake(sizeMain.size.width, -100, _ongTren1.frame.size.width, _ongTren1.frame.size.height);
 
-    _ongTren2.frame=CGRectMake(_ongTren1.frame.origin.x+290, 0, 90, 277);
+    _ongTren2.frame=CGRectMake(_ongTren1.frame.origin.x+290, -223, _ongTren2.frame.size.width, _ongTren2.frame.size.height);
 
-    _ongTren3.frame=CGRectMake(_ongTren2.frame.origin.x+290, 0, 90, 277);
+    _ongTren3.frame=CGRectMake(_ongTren2.frame.origin.x+290, -450, _ongTren3.frame.size.width, _ongTren3.frame.size.height);
     
-    _ongDuoi1.frame=CGRectMake(_ongTren1.frame.origin.x, _ongTren1.frame.size.height+100, _ongTren1.frame.size.width, _ongTren1.frame.size.height);
+    _ongDuoi1.frame=CGRectMake(_ongTren1.frame.origin.x,_ongTren1.frame.origin.y
+                               +_ongTren1.frame.size.height+100, _ongDuoi1.frame.size.width, _ongDuoi1.frame.size.height);
     
-    _ongDuoi2.frame=CGRectMake(_ongTren2.frame.origin.x,_ongTren2.frame.size.height+100,  _ongDuoi2.frame.size.width,  _ongDuoi2.frame.size.height);
+    _ongDuoi2.frame=CGRectMake(_ongTren2.frame.origin.x,_ongTren2.frame.origin.y
+                               +_ongTren2.frame.size.height+100,  _ongDuoi2.frame.size.width,  _ongDuoi2.frame.size.height);
 
-    _ongDuoi3.frame=CGRectMake(_ongTren3.frame.origin.x, _ongTren3.frame.size.height+100,  _ongDuoi3.frame.size.width,  _ongDuoi3.frame.size.height);
+    _ongDuoi3.frame=CGRectMake(_ongTren3.frame.origin.x, _ongTren3.frame.origin.y
+                               +_ongTren3.frame.size.height+100,  _ongDuoi3.frame.size.width,  _ongDuoi3.frame.size.height);
 
     bird=[[UIImageView alloc] initWithFrame:CGRectMake(20,sizeMain.size.height/2,34,24)];
     bird.animationImages=@[
@@ -119,24 +182,6 @@
     [self.view addSubview:bird];
     [bird startAnimating];
     
-    UITapGestureRecognizer* gs=[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(birdFlight)];
 }
--(void)birdFlight{
-    
-}
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
